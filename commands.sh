9a701be76d391aa1fcc47cb085f1add7aef3fadb
Siddartha <your.email@example.com>
2026-02-14 17:28:31 +0530

    updated files with the information

--- commands.sh
+++ commands.sh
@@ -3,17 +3,53 @@
 # Update system
 sudo apt update && sudo apt upgrade -y
 
-# Install Nginx
-sudo apt install nginx -y
+# Install Apache2
+sudo apt install Apache2
 
-# Install PHP and extensions
-sudo apt install php-fpm php-mysql -y
+# create a file name index.php
+sudo nano index.php
 
-# Configure Nginx to use PHP
-sudo nano /etc/nginx/sites-available/default
+# create a root web directory to store the site
+cd /var/www/html
 
-# Restart Nginx
-sudo systemctl restart nginx
+# Install PHP Dependencies & Extensions
+sudo apt install php8.3
 
-# Test PHP
-echo "<?php phpinfo(); ?>" | sudo tee /var/www/html/info.php
+# install my sql server && client  
+sudo apt install mysql-server
+sudo apt install mysql-client
+
+sudo apt install  php8.3-mysqli
+
+# add the repository of php code
+sudo add-apt-repositoty -y ppa:ondrej/php
+
+# check status & Restart the Apache2 server
+sudo systemctl restart Apache2
+sudo systemctl status Apache2
+
+# connect EC2 instance with mysql database with RDS Endpoint
+
+mysql -h <end-point> -u  <username> -p<password>
+
+#  In sql server run these commands
+
+show databases; # this  shows available databases on server
+
+# select your database, eg: intel
+
+USE intel;
+
+show tables; # this shows tables available in database
+
+
+# if data or table aren't available 
+
+# this will create a table  name "data" 
+#  create a table with name
+Create table data(firstname varchar(50), gmail varchar(50));
+
+# to show the data inside tables
+select * from data
+
+## ... thats  end of commands... ##
